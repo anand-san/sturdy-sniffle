581bbab0d68d9c72766689d32793f79cc64263f2
Anand <seanjin17@gmail.com>
2023-08-23 14:01:52 +0200

    feat: get input table from csv

--- src/main.test.ts
+++
@@ -1,5 +0,0 @@
-import { sum } from './main'
-
-test('basic', () => {
-  expect(sum()).toBe(0)
-})
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,39 @@
-export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0)
+import { parse } from 'csv-parse'
+import { possibleTable } from './utils'
+import { rotateTableLeft } from './rotate-table'
+import fs from 'fs'
+
+const main = () => {
+  try {
+    const csvPath = process.argv[2]
+
+    if (!csvPath) throw 'no file path provided'
+
+    if (!csvPath.includes('.csv')) throw 'invalid file'
+
+    fs.createReadStream(csvPath)
+      .pipe(parse({ delimiter: ',', from_line: 2 }))
+      .on('data', function (row: string[]) {
+        const index = row[0]
+        const originalTable = row[1]
+        const originalTableObject = JSON.parse(originalTable) as number[]
+        const isRotationPossible = possibleTable(originalTableObject.length)
+
+        const rotatedTable = rotateTableLeft({ table: originalTableObject })
+
+        console.log(
+          `${index}, ${JSON.stringify(rotatedTable)}, ${isRotationPossible}`
+        )
+      })
+      .on('end', function () {
+        // console.log('There you go')
+      })
+      .on('error', function (error: Error) {
+        console.log('Error', error.message)
+      })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+main()
