c99a4a6dbe072e10894d3f4934631ff5163c6450
Anand <seanjin17@gmail.com>
2023-08-23 12:51:55 +0200

    feat: more configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,34 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: 'standard-with-typescript',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script',
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: 'typescript.eslintrc.json'
   },
-  rules: {},
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    // to enforce using type for object type definitions, can be type or interface
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type']
+  }
 }
