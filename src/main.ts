84f22ee8e2b0cb1adab2ae1f282abe2f2ab050d5
Anand Sandilya <anand.sandilya@quantilope.com>
2023-08-25 05:45:59 +0200

    feat: handle more input errors

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,23 @@ const main = () => {
 
     if (!csvPath) throw 'no file path provided'
 
-    if (!csvPath.includes('.csv')) throw 'invalid file'
+    if (!csvPath.includes('.csv'))
+      throw 'I only understand csv files, did you miss adding the .csv file extension at the end of input?'
+
+    if (!fs.existsSync(csvPath)) throw 'file not found'
 
     fs.createReadStream(csvPath)
       .pipe(parse({ delimiter: ',', from_line: 2 }))
       .on('data', function (row: string[]) {
         const index = row[0]
         const originalTable = row[1]
-        const originalTableObject = JSON.parse(originalTable) as number[]
+        let originalTableObject: number[] = []
+        try {
+          originalTableObject = JSON.parse(originalTable) as number[]
+        } catch {
+          // Invalid List
+          console.log(`${index}, [], false`)
+        }
         const isRotationPossible = possibleTable(originalTableObject.length)
 
         const rotatedTable = rotateTableLeft({ table: originalTableObject })
